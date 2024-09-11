8b88cd32a9c16e061f0cded7760a1da3889864ea
Michael Li <michaelli@crc-dot1x-nat-10-239-209-147.bu.edu>
2024-09-11 17:21:10 -0400

    initial commit

---
+++ polynomial.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 17:07:32 2024
+
+@author: michaelli
+"""
+
+class X:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "X"
+    
+    def evaluate(self, i):
+        self.i=i
+
+class Int:
+    def __init__(self, i):
+        self.i = i
+    
+    def __repr__(self):
+        return str(self.i)
+    
+    
+
+class Add:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, i):
+        p1 = self.p1.evaluate(i)
+        p2 = self.p2.evaluate(i)
+        return Int(p1.i + p2.i)
+        
+class Mul:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        if isinstance(self.p1, Add):
+            if isinstance(self.p2, Add):
+                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+        if isinstance(self.p2, Add):
+            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " * " + repr(self.p2)
+
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly)
